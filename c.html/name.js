64908aa7bcc5db016f8212d404cdfbdc06d07d2a
Selvanatadros <108242469+Selvanatadros@users.noreply.github.com>
2023-09-17 16:44:46 +0300

    Add files via upload

---
+++ c.html/name.js
@@ -0,0 +1,9 @@
+function toggler(e) {
+   if( e.innerHTML == 'Show' ) {
+       e.innerHTML = 'Hide'
+       document.getElementById('password').type="text";
+   } else {
+       e.innerHTML = 'Show'
+       document.getElementById('password').type="password";
+   }
+ }
